b846548346e389a47eb693a00885c7e700f80fb3
Andrew_Ermolenko <Shpinatakos@mail.ru>
2023-04-08 20:18:56 +0700

    Добавил поиск по заметкам и печать в консоль

--- Controller.py
+++ Controller.py
@@ -17,7 +17,7 @@ def Create_note():
     with open("notes.csv", "a", encoding='utf-8') as file:
         file.write(f'{note}\n')
     file.close()
-    print(f"\nЗаметка сохранена под номером {id}\n")
+    print(f"\nЗаметка сохранена под номером {id}")
 
 
 def Read_db():
@@ -38,7 +38,67 @@ def Read_db():
             temp_note[temp[0]] = val
             notes.append(temp_note)
         file.close()
+    print(notes)
     return notes
 
 
-Create_note()
+def Print_note(note):
+    result = []
+    for keys, val in note.items():
+        result.append(val[0])
+        result.append(val[1])
+        result.append(val[2])
+        if len(val) > 3:
+            result.append(val[3])
+    if len(result) == 0:
+        print("\nЗаписи с таким номер не существует")
+    else:
+        print(f"\nЗапись №{keys}\nНазвание:{result[0]}\nТекст заметки:{result[1]}\nДата создания:{result[2]}")
+    if len(note.values()) == 4:
+        print(f"Дата последнего изменения: {result[3]}")
+
+
+def Find():
+    action = input(
+        "Выберете метод поиска:\n1 - по номеру:\n2 - по названию:\n3 - по содержимому заметки:"
+        "\n4 - по дате:\n5 - выход в главное меню:\n6 - Выход из программы\nВведите цифру действия --> ")
+    while (int(action) < 1 or int(action) > 6):
+        action = input("ВЫБРАН НЕ ВЕРНЫЙ ПУНКТ !!!\nВведите верный пункт меню ")
+    match action:
+        case '1':
+            action = input("Введите номер заметки -->")
+            db = Read_db()
+            for item in db:
+                for key, val in item.items():
+                    if action == key:
+                        Print_note(item)
+        case '2':
+            action = input("Введите название заметки -->")
+            db = Read_db()
+            for item in db:
+                for key, val in item.items():
+                    if action in val[0]:
+                        Print_note(item)
+        case '3':
+            action = input("Введите содержимое заметки -->")
+            db = Read_db()
+            for item in db:
+                for key, val in item.items():
+                    if action in val[1]:
+                        Print_note(item)
+        case '4':
+            action = input("Введите число, либо месяц, либо год создания заметки -->")
+            db = Read_db()
+            for item in db:
+                for key, val in item.items():
+                    if action in val[2]:
+                        Print_note(item)
+        case '5':
+            print('Переход в главное меню')
+        case '6':
+            exit()
+
+
+# Create_note()
+# Read_db()
+# Find()
